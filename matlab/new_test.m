7dabc0be33a7823bdde67f088fb8c9bc9ae6a51e
stoertebeker23 <becluki@web.de>
2017-04-30 12:30:35 +0200

    generate output buffer or at least try

--- matlab/new_test.m
+++ matlab/new_test.m
@@ -2,18 +2,22 @@
 pi = 3.1415926;
 debug = 0; % debug mode, show several plotted matrices
 
+%% frequencies
 fs = 48000; % sampling frequency
 fc = 12000; % center frequency
 
 Ts = 1/fs;
+f_sig = 4000; % signal frequency in Hz
 
 A = 1; % signal amplitude
 signal_time_step = Ts;
 t = 4; % signal sample length in sigure;
 time_array = (0:signal_time_step:t-signal_time_step);
-f_sig = 7000; % signal frequency in Hz
+
 lut_table_size = 4800; % length for the cosine lookup table
 buffer_length = 32; % length of ping and pong buffers
+
+k = 5; % frequency deviation
 
 %% generate signal
 signal = A*cos(2*pi*f_sig*time_array-pi/2);
@@ -39,19 +43,29 @@ end;
 
 %% read input signal to buffer
 
-buffer = [1:1:buffer_length;1:1:buffer_length];
+inputbuffer = [1:1:buffer_length;1:1:buffer_length];
+outputbuffer = [1:1:buffer_length;1:1:buffer_length];
+
 n = 1;
 for r = 1:1:20;
-    n = not(n-1)+1;
+    
     for i = 1:1:buffer_length;
-        buffer(n,i) = signal(r*32+i);
+        inputbuffer(n,i) = signal(r*32+i);
+    end;
+    
+    n = not(n-1)+1; % change buffer from ping to pong or the other way
+    
+    for i = 1:1:buffer_length;
+        % do sth
+        nT = r*32+i % sample time
+        outputbuffer(n,i) = cos(2*pi*k*nT*sum(signal(1:1:nT)))
     end;
 end;
 
 figure;
 subplot(2,1,1)
-plot(0:1:31,buffer(1,1:1:32));
+plot(0:1:31,inputbuffer(1,1:1:32));
 title('last input ping buffer contents')
 subplot(2,1,2)
-plot(0:1:31,buffer(2,1:1:32));
+plot(0:1:31,outputbuffer(2,1:1:32));
 title('last input pong buffer contents')
